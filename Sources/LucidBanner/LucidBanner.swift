addd84e49811be51647b2a7bbad0843091019401
Marino Faggiana <marino.faggiana@nextcloud.com>
2025-12-11 16:55:55 +0100

    Update LucidBanner.swift

--- Sources/LucidBanner/LucidBanner.swift
+++ Sources/LucidBanner/LucidBanner.swift
@@ -82,7 +82,7 @@ public final class LucidBanner: NSObject, UIGestureRecognizerDelegate {
         /// Absolute point in window coordinates.
         case absolute(CGPoint)
         /// Attach to one of the window corners with a given inset.
-        case corner(Corner, inset: CGSize = CGSize(width: 20, height: 40))
+        case corner(Corner, inset: CGSize)
         /// Supported corners for minimized placement.
         public enum Corner {
             case topLeading
